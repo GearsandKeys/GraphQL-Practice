b62cfbd5d31c356587d0099d13ff647e158aa43c
Nathan Gearke <n.gearke@gmail.com>
2021-05-04 21:44:14 -0500

    Finished tutorial, learned about mutations!

--- server.js
+++ server.js
@@ -107,8 +107,42 @@ const RootQueryType = new GraphQLObjectType({
     })
 })
 
+const RootMutationType = new GraphQLObjectType ({
+    name: 'Mutation',
+    description: 'Root Mutation',
+    fields: () => ({
+        addBook: {
+            type: BookType,
+            description: 'Add a book',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                authorId: { type: GraphQLNonNull(GraphQLInt) },
+            },
+            resolve: (parent, args) => {
+                const book = { id: books.length + 1, name: args.name, authorId: args.authorId} //DB would automatically generate the ID
+                books.push(book) //add book to our array
+                return book
+            }
+            
+        },
+        addAuthor: {
+            type: AuthorType,
+            description: 'Add an author',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) }
+            },
+            resolve: (parent, args) => {
+                const author = { id: authors.length + 1, name: args.name} //DB would automatically generate the ID
+                authors.push(author) //add author to our array
+                return author
+            }
+        }
+    })
+})
+
 const schema = new GraphQLSchema ({
-    query: RootQueryType
+    query: RootQueryType,
+    mutation: RootMutationType
 })
 
 app.use('/graphql', expressGraphQL({
